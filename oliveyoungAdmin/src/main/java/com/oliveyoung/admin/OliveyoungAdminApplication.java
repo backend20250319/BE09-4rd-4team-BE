2bb0910f1e7c5856e06e8ac72d55c186fa00d869
jieun <yudaeun222@gmail.com>
2025-07-02 14:17:04 +0900

    first commit

---
+++ oliveyoungAdmin/src/main/java/com/oliveyoung/admin/OliveyoungAdminApplication.java
@@ -0,0 +1,13 @@
+package com.oliveyoung.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliveyoungAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OliveyoungAdminApplication.class, args);
+	}
+
+}
