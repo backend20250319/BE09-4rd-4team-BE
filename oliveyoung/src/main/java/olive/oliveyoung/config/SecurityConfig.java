f8d1290a8879dbb0e10bbafab48eac824ba7d637
parkchangjun <parkchangjun13@gmail.com>
2025-07-09 15:10:00 +0900

    SecurityConfig update

--- oliveyoung/src/main/java/olive/oliveyoung/config/SecurityConfig.java
+++ oliveyoung/src/main/java/olive/oliveyoung/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import olive.oliveyoung.config.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
